bef52a8344994cb735e610255d9fd2d43cbd2628
USER <j20951948@gmail.com>
2019-10-21 23:25:33 +0800

    first commit

--- pythonScraping/Ch2/regexTest.py
+++ pythonScraping/Ch2/regexTest.py
@@ -7,3 +7,12 @@ bs = BeautifulSoup(html, 'html.parser')
 images = bs.find_all('img', {'src':re.compile('\.\.\/img\/gifts/img.*\.jpg')})
 for image in images: 
     print(image['src'])
+
+x = bs.find_all(lambda tag: len(tag.attrs) == 2) #找出有兩個屬性的標籤
+print("\nlen(tag.attrs) == 2:\n", x, "\n")
+
+x = bs.find_all(lambda tag: tag.get_text() == 'Or maybe he\'s only resting?')
+print("print'Or maybe he\'s only resting?':\n", x, "\n")
+
+x = bs.find_all('', text='Or maybe he\'s only resting?')
+print("print'Or maybe he\'s only resting?':\n", x, "\n")
