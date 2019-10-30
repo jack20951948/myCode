17cf13c2e413ff78b0e680f0bbddc599ba6dae92
USER <j20951948@gmail.com>
2019-10-31 00:25:38 +0800

    add json file

--- Lab_web/main.js
+++ Lab_web/main.js
@@ -132,3 +132,21 @@ function page(id,tag)
   }
 
 }
+
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+//usage:
+readTextFile("/code/Lab_web/material/test.json", function(text){
+  var data = JSON.parse(text);
+  console.log(data);
+});
