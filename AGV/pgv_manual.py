4cdd56a835c8f526de092d2ab222afb209df6acd
jack20951948 <j20951948@gmail.com>
2020-07-27 15:01:39 +0800

    pgv manual

--- AGV/pgv_manual.py
+++ AGV/pgv_manual.py
@@ -10,6 +10,12 @@ import subprocess as sp
 
 import RPi.GPIO as GPIO
 from adafruit_servokit import ServoKit
+
+import sys
+import termios
+import atexit
+from select import select
+from keyboard_extention import KBHit
     
 class pgv_action():
     def checkSpeed(sp): # 检查避免超出PWM产生器范围
@@ -20,7 +26,9 @@ class pgv_action():
         return sp
 
     def stop_agv(breaker=True):
+        global kit, right_speed, left_speed, event
         GPIO.output(stopPin, GPIO.HIGH)
+        GPIO.output(rollerRun, GPIO.LOW)
         kit.servo[0].angle = 0
         kit.servo[1].angle = 0
         if breaker:
@@ -28,7 +36,8 @@ class pgv_action():
         else:
             GPIO.output(brakePin, GPIO.LOW)
 
-    def move_forward(final_speed=10, time_constant=10, acc=1):
+    def move_forward(final_speed=20, acc=1):
+        global kit, right_speed, left_speed, event
         GPIO.output(stopPin, GPIO.LOW)
         GPIO.output(brakePin, GPIO.LOW)
 
@@ -44,7 +53,8 @@ class pgv_action():
 
         time.sleep(0.01)
 
-    def move_backward(final_speed=10, time_constant=10, acc=1):
+    def move_backward(final_speed=20, acc=1):
+        global kit, right_speed, left_speed, event
         GPIO.output(stopPin, GPIO.LOW)
         GPIO.output(brakePin, GPIO.LOW)
 
@@ -60,7 +70,25 @@ class pgv_action():
 
         time.sleep(0.01) 
 
-    def move_left(final_speed=10, time_constant=10, acc=1):
+    def move_F_left(final_speed=20, acc=2):
+        global kit, right_speed, left_speed, event
+        GPIO.output(stopPin, GPIO.LOW)
+        GPIO.output(brakePin, GPIO.LOW)
+
+        GPIO.output(rightDirPin, GPIO.LOW)
+        GPIO.output(leftDirPin, GPIO.HIGH)
+
+        if right_speed < final_speed:
+            right_speed += acc
+        if left_speed < (final_speed / 2):
+            left_speed += (acc / 2)
+        kit.servo[0].angle = pgv_action.checkSpeed(right_speed)
+        kit.servo[1].angle = pgv_action.checkSpeed(left_speed)
+
+        time.sleep(0.01) 
+
+    def move_B_left(final_speed=20, acc=2):
+        global kit, right_speed, left_speed, event
         GPIO.output(stopPin, GPIO.LOW)
         GPIO.output(brakePin, GPIO.LOW)
 
@@ -76,7 +104,25 @@ class pgv_action():
 
         time.sleep(0.01) 
 
-    def move_right(final_speed=10, time_constant=10, acc=1):
+    def move_F_right(final_speed=20, acc=2):
+        global kit, right_speed, left_speed, event
+        GPIO.output(stopPin, GPIO.LOW)
+        GPIO.output(brakePin, GPIO.LOW)
+
+        GPIO.output(rightDirPin, GPIO.LOW)
+        GPIO.output(leftDirPin, GPIO.HIGH)
+
+        if right_speed < (final_speed / 2):
+            right_speed += (acc / 2)
+        if left_speed < final_speed:
+            left_speed += acc
+        kit.servo[0].angle = pgv_action.checkSpeed(right_speed)
+        kit.servo[1].angle = pgv_action.checkSpeed(left_speed)
+
+        time.sleep(0.01) 
+
+    def move_B_right(final_speed=20, acc=2):
+        global kit, right_speed, left_speed, event
         GPIO.output(stopPin, GPIO.LOW)
         GPIO.output(brakePin, GPIO.LOW)
 
@@ -93,22 +139,45 @@ class pgv_action():
         time.sleep(0.01) 
     
     def rolling_L():
-        GPIO.output(rollerRun, GPIO.HIGH)
-        GPIO.output(rollerDir,GPIO.LOW)
-
         GPIO.output(rollerRun, GPIO.LOW)
         GPIO.output(rollerDir,GPIO.HIGH)
+        #time.sleep(1.0)
         GPIO.output(rollerRun, GPIO.HIGH)
         time.sleep(0.01)
 
     def rolling_R():
         GPIO.output(rollerRun, GPIO.LOW)
         GPIO.output(rollerDir,GPIO.LOW)
+        #time.sleep(1.0)
         GPIO.output(rollerRun, GPIO.HIGH)
         time.sleep(0.01)
 
+    def slowly_stop():
+        global kit, right_speed, left_speed, event
+        # GPIO.output(rollerRun, GPIO.LOW)
+        if left_speed == right_speed:
+            if right_speed > 0:
+                right_speed -= 0.2
+            else:
+                right_speed = 0
+            if left_speed > 0:
+                left_speed -= 0.2
+            else:
+                left_speed = 0
+        else:
+            if right_speed > left_speed:
+                right_speed -= 0.2
+                left_speed -= 0.1
+            else:
+                right_speed -= 0.1
+                left_speed -= 0.2
+        kit.servo[0].angle = pgv_action.checkSpeed(right_speed)
+        kit.servo[1].angle = pgv_action.checkSpeed(left_speed)
+        time.sleep(0.01)
+
+
     def goMotorsLoop():
-        global kit, right_speed, left_speed
+        global kit, right_speed, left_speed, event
 
         right_speed = 0
         left_speed = 0
@@ -116,21 +185,45 @@ class pgv_action():
         print('move!')
         while True:
             if event == 'p':
+                # print("stop")
                 pgv_action.stop_agv()
+
             elif event == 'w':
+                # print("move_forward")
                 pgv_action.move_forward()
+
             elif event == 's':
+                # print("move_backward")
                 pgv_action.move_backward()
+
+            elif event == 'q':
+                # print("move_F_left")
+                pgv_action.move_F_left()
+
             elif event == 'a':
-                pgv_action.move_left()
+                # print("move_B_left")
+                pgv_action.move_B_left()
+
+            elif event == 'e':
+                # print("move_F_right")
+                pgv_action.move_F_right()
+
             elif event == 'd':
-                pgv_action.move_right()
-            else:
-                GPIO.output(rollerRun, GPIO.LOW)
-                if right_speed > 0:
-                    right_speed -= 0.5
-                if left_speed > 0:
-                    left_speed -= 0.5
+                # print("move_B_right")
+                pgv_action.move_B_right()
+
+            elif event == 'm':
+                # print("rolling_L")
+                pgv_action.rolling_L()
+
+            elif event == 'n':
+                # print("rolling_R")
+                pgv_action.rolling_R()
+
+            if event == 'null':
+                # print("else")
+                pgv_action.slowly_stop()
+                
 
     # --- move Motors end ---
 
@@ -193,13 +286,18 @@ class GPIO_setting():
 
 class keyboard():
     def readkeyboard():
-        global event
-        event = getch.getch().lower()
-        if event != "w" and event != "a" and event != "s" and event != "d" and event != "n" and event != "m" and event != "p":
-            event = None
+        global event, kb
+        if kb.kbhit():
+            event = kb.getch()
+        else:
+            event='null'
     def keyboardLoop():
+        global kb
         while True:
-            readkeyboard()
+            keyboard.readkeyboard()
+            # print(event)
+            time.sleep(0.02)
+        kb.set_normal_term()
 
 def setup_servoKit():
     global kit
@@ -218,6 +316,7 @@ def Setup():
 
 if __name__ == "__main__":
     Setup()
+    kb = KBHit()
 
     readkeyboardThread = threading.Thread(name="readkeyboard", target=keyboard.keyboardLoop)
     readkeyboardThread.start()
