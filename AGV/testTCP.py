fa20c4db4f224f0ffffe38eb070042b7d4944983
USER <j20951948@gmail.com>
2020-12-13 01:34:35 +0800

    TC

--- AGV/testTCP.py
+++ AGV/testTCP.py
@@ -2,11 +2,12 @@ import socket               # 导入 socket 模块
  
 s = socket.socket()         # 创建 socket 对象
 host = socket.gethostname() # 获取本地主机名
-port = 1000                # 设置端口
+port = 1025                # 设置端口
 s.bind((host, port))        # 绑定端口
  
 s.listen(5)                 # 等待客户端连接
 while True:
+    # print(host)
     c,addr = s.accept()     # 建立客户端连接
     print('连接地址：', addr)
     data = c.recv(1024)  #接收数据
